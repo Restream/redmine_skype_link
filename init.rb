34124b61a7353e41d4a55a8e8721c28bf3304eb2
Irina Lebedevich <irina.lebedevich@undev.ru>
2015-01-16 13:40:56 +0300

    ref #56811 Init.rb updated

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'redmine'
 require 'redmine_skype_link'
 
 Redmine::Plugin.register :redmine_skype_link do
-  name 'Redmine Skype Link plugin'
-  author 'danil.tashkinov@gmail.com'
-  description 'Add skype links to redmine wiki formatting'
+  name 'Redmine Skype Link Plugin'
+  author 'Undev'
+  description 'This plugin allows using Skype URIs in Redmine.'
   version '0.0.1'
   url 'https://github.com/Undev/redmine_skype_link'
   author_url 'http://github.com/Undev'
