c4d29b1a79a72f026e72c36d207fdff120534d7a
Danil Tashkinov <danil.tashkinov@gmail.com>
2015-04-28 11:17:06 +0300

    Bump version to 1.0.0

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_skype_link do
   name 'Redmine Skype Link Plugin'
   author 'Undev'
   description 'This plugin allows using Skype URIs in Redmine.'
-  version '0.0.1'
+  version '1.0.0'
   url 'https://github.com/Undev/redmine_skype_link'
   author_url 'http://github.com/Undev'
   requires_redmine :version_or_higher => '2.1'
